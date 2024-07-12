dd3006c7f899b08c86194a0f6eb4c0e1ed04d34a
SHRYNSH-NETAM <netams2000@gmail.com>
2024-07-12 20:15:23 +0530

    Added Authorization method for handling paths like /signin, /signup and /delete

--- routes/userAuth.go
+++ routes/userAuth.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"net/http"
 
+	controller "github.com/SHRYNSH-NETAM/Sudoku_Backend/controllers"
 	"github.com/SHRYNSH-NETAM/Sudoku_Backend/middleware"
 	"github.com/go-chi/chi/v5"
 )
@@ -11,26 +12,14 @@ func UserAuthRouter() http.Handler {
 	r := chi.NewRouter()
 
 	r.Group(func(r chi.Router) {
-		r.Post("/signin", Signin)
-		r.Post("/signup", Signup)
+		r.Post("/signin", controller.Signin)
+		r.Post("/signup", controller.Signup)
 	})
 
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.JwtAuth)
-		r.Delete("/deleteAccount", DeleteAccount)
+		r.Delete("/deleteAccount", controller.DeleteAccount)
 	})
 
 	return r
-}
-
-func DeleteAccount(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte(""))
-}
-
-func Signin(w http.ResponseWriter, r *http.Request){
-
-}
-
-func Signup(w http.ResponseWriter, r *http.Request){
-	
 }
